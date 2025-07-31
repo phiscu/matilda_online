599477e9609ff13768ebef12d5ef7e8c3e85b7ec
hoepke <miajanzen@gmx.de>
2025-07-31 16:56:38 +0200

    changed rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ autodoc_mock_imports = [
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = "_static/matilda_cupcake.png"
 # html_theme_options = {
