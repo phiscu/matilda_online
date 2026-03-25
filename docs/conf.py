a8b181b4f70f00332194c2b640540617ff23b40c
Phillip Schuster <phillip.schuster@posteo.de>
2026-03-25 21:00:14 +0100

    Prepare release v1.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 
 project = 'Matilda ONLINE'
-copyright = '2025, Alexander Georgi, Phillip Schuster, Mia Janzen'
+copyright = '2026, Alexander Georgi, Phillip Schuster, Mia Janzen'
 author = 'Alexander Georgi, Phillip Schuster, Mia Janzen'
-release = '1.0.0'
+release = '1.0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
